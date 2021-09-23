00a1d9528fe8ad324ff18911f71cf9a3b9573661
amyhaber <amyhaber@users.noreply.github.com>
2021-09-23 13:47:19 +0800

    add comments for [os_support] module to make it easier to add implementation for the other platforms support

--- source/os_support.h
+++ source/os_support.h
@@ -4,14 +4,23 @@
 #include <string>
 
 //
-// check if the given os + min_required_version is accepted by the current running platform
+// check if the given [osMinRequiredVersion] is accepted by the current running platform
 // 
 bool is_acceptable_os_version(const std::string& osMinRequiredVersion);
 
+//
+// check if the name of os is matched
+// 
 bool is_matched_os_name(const std::string& osName);
 
+//
+// execute an executable file (PE/ELF) with the given argument string
+// 
 bool execute(const std::string& package, const std::string& args);
 
+//
+// get the language (ISO-639 code) setting of the current OS user
+// 
 std::string get_iso639_user_lang();
 
 #endif //_OS_SUPPORT_H_
